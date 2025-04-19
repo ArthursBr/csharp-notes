dee085b0b58be20a90c4d831d848ee772b2f3508
Arthur <arthur.soares.barbosa.leal7@gmail.com>
2025-04-19 16:19:18 -0300

    P.O.O com alunos.

---
+++ ex3/ex3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ex3 {
+    class Program {
+        static void Main(string[] args) {
+            Aluno a = new Aluno();
+            Console.Write("Nome do aluno: ");
+            a.Nome = Console.ReadLine();
+            Console.WriteLine("Informe as tres notas do aluno: ");
+            a.Primeira = double.Parse(Console.ReadLine());
+            a.Segunda = double.Parse(Console.ReadLine());
+            a.Terceira = double.Parse(Console.ReadLine());
+
+            double final = a.SomaTotal();
+            a.ResultadoFinal(final);
+
+        }
+    }
+}
---
+++ ex3/ex3/aluno.cs
@@ -0,0 +1,26 @@
+﻿using System.Globalization;
+using System.Xml;
+
+namespace ex3 {
+    class Aluno {
+        public string Nome;
+        public double Primeira;
+        public double Segunda;
+        public double Terceira;
+
+        public double SomaTotal() {
+            return (Primeira +  Segunda + Terceira);
+        }
+
+        public void ResultadoFinal(double nota) {
+            Console.WriteLine($"NOTA FINAL = {nota}");
+            if(nota < 60) {
+                double faltou = 60 - nota;
+                Console.WriteLine("REPROVADO\nFALTARAM " + faltou.ToString("F2", CultureInfo.InvariantCulture) + " PRONTOS");
+            }
+            else {
+                Console.WriteLine("APROVADO");
+            }
+        }
+    }
+}
