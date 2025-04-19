9863d0d1f593536e8dc81c5c6337a801da1d6770
Arthur <arthur.soares.barbosa.leal7@gmail.com>
2025-04-19 16:17:05 -0300

    P.O.O com área geométrica

---
+++ ex1/ex1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using ex1;
+
+namespace Ex1 {
+    class Program {
+        static void Main(string[] args) {
+            Retangulo r = new Retangulo();
+            Console.WriteLine("Entre a largura e altura do retângulo:");
+            r.Altura = double.Parse(Console.ReadLine());
+            r.Largura = double.Parse(Console.ReadLine());
+            Console.WriteLine($"Area: {r.Area()}");
+            Console.WriteLine($"Perimetro: {r.Perimetro():F2}");
+            Console.WriteLine($"Diagonal: {r.Diagonal():F2}");
+        }
+    }
+}
