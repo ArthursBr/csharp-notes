0f7dae330eaede4dd67c191dc9c8d8bbf432d499
Arthur <arthur.soares.barbosa.leal7@gmail.com>
2025-04-19 16:18:24 -0300

    P.O.O com funcionários.

---
+++ ex2/ex2/Funcionario.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+
+namespace ex2 {
+    class Funcionario {
+        public string Name;
+        public double Salario;
+        public double Imposto;
+
+        public double SalarioLiquido() {
+            return Salario - Imposto;
+        }
+
+        public void AumentarSalario(int porcentagem) {
+            Salario = (Salario * porcentagem / 100) + Salario;
+        }
+
+        public override string ToString() {
+            return Name + ", $ " + SalarioLiquido().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ ex2/ex2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ex2 {
+    class Program {
+        static void Main(string[] args) {
+            Funcionario f = new Funcionario();
+            Console.Write("Nome: ");
+            f.Name = Console.ReadLine();
+            Console.Write("Salario bruto: ");
+            f.Salario = double.Parse(Console.ReadLine());
+            Console.Write("Imposto: ");
+            f.Imposto = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"\nFuncionario: {f}");
+
+            Console.Write("\nDigite a porcentagem para aumentar o salário: ");
+            int aum = int.Parse(Console.ReadLine());
+
+            f.AumentarSalario(aum);
+            Console.WriteLine($"\nDados atualizados: {f}");
+        }
+    }
+}
