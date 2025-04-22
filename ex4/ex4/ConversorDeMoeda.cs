7bae37e93720cb9f114087cd544f4db239f6d792
Arthur <arthur.soares.barbosa.leal7@gmail.com>
2025-04-22 18:12:06 -0300

    P.O.O com moedas.

---
+++ ex4/ex4/ConversorDeMoeda.cs
@@ -0,0 +1,12 @@
+﻿using System.Globalization;
+namespace ex4 {
+    class ConversorDeMoeda {
+        public static double Cotacao;
+        public static double Dolar;
+
+        public static double TotalEmReais() {
+            double total = Cotacao * Dolar;
+            return total + (total * 6 / 100.0);
+        }
+    }
+}
---
+++ ex4/ex4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Globalization;
+
+namespace ex4 {
+    public class Program {
+        static void Main(string[] args) {
+
+            Console.Write("Qual é a cotação do dólar? ");
+            ConversorDeMoeda.Cotacao = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantos dólares você vai comprar? ");
+            ConversorDeMoeda.Dolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Valor a ser pago em reais: " + ConversorDeMoeda.TotalEmReais().ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
