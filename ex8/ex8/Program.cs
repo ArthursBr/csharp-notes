dd1e1c7607807681630be304b2d75bd9f648bfb9
Arthur <arthur.soares.barbosa.leal7@gmail.com>
2025-05-14 17:05:26 -0300

    lista2

---
+++ ex8/ex8/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ex8 {
+    class Program {
+        static void Main(string[] args) {
+            Console.Write("Informe TAM: ");
+            int TAM = int.Parse(Console.ReadLine());
+            int[,] matriz = new int[TAM, TAM];
+            for (int i = 0; i < matriz.GetLength(0); i++) {
+                string[] values = Console.ReadLine().Split(' ');
+                for (int f = 0; f < matriz.GetLength(1); f++) {
+                    matriz[i,f] = int.Parse(values[f]);
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("Main diagonal: ");
+            for (int i = 0; i < matriz.GetLength(0); i++) {
+                Console.Write(matriz[i,i] + " ");
+            }
+
+            Console.WriteLine();
+            int count = 0;
+            for (int i = 0; i < matriz.GetLength(0); i++) {
+                for (int f = 0; f < matriz.GetLength(1); f++) {
+                    if (matriz[i,f] < 0) count++;
+                }
+            }
+            Console.WriteLine("Negative numbers: " + count);
+        }
+    }
+}
